2fb0f8a77ff37a960c44e8103d6e8238e718e1a5
amimanye <amimanyeaglago@gmail.com>
2023-06-16 02:39:19 +0000

    printf function for display

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main: entry point
+ * main - Entry point
  *
- * return: always 0 (success)
+ * Return: always 0 (success)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
