beb7e8c10046872e02362e5fbf8669dd5f013983
amimanye <amimanyeaglago@gmail.com>
2023-06-16 03:07:21 +0000

    size of operator

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-	printf("Size of a char: %lu bytes(s)\n", sizeof(char));
-	printf("Size of a int: %lu bytes(s)\n", sizeof(int));
-	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
-	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
-	printf("Size of a float: %lu bytes(s)\n", sizeof(float));
+	printf("Size of a char: %lu bytes(s)", sizeof(char));
+	printf("Size of an int: %lu bytes(s)", sizeof(int));
+	printf("Size of a long int: %lu bytes(s)", sizeof(long int));
+	printf("Size of a long long int: %lu bytes(s)", sizeof(long long int));
+	printf("Size of a float: %lu bytes(s)", sizeof(float));
 	return (0);
 }
