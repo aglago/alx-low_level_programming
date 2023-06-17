89da7ebe186ed46cb6dc039846addba3f1b545d0
amimanye <amimanyeaglago@gmail.com>
2023-06-17 00:11:53 +0000

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for program
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+
+	unsigned char alphabet = 97;
+
+	while (alphabet <= 122)
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	return (0);
+}
