a47a91c00dab5e3a67c6cadd3b1d4e88afe398db
amimanye <amimanyeaglago@gmail.com>
2023-06-19 05:10:39 -0700

    print to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - prinzs to standard error
+ * Return: always 0 if successful
+ */
+
+int main(void)
+{
+	int file_dsc = open("/dev/stderr", O_WRONLY);
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(file_dsc, message, strlen(message));
+	close(file_dsc);
+	return (0);
+}
