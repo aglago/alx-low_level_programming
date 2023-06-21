a5096f747490448fcc0df09f42e52474712c55fe
amimanye <amimanyeaglago@gmail.com>
2023-06-20 21:58:51 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - prints absolute value
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	int absolute;
+	
+	if (n < 0)
+		absolute = (-) * n;
+	else
+		absolute = n;
+	return aboslute;
+}
