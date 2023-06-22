f2392aa947de795938e07a94d63116c148ea1c8d
amimanye <amimanyeaglago@gmail.com>
2023-06-22 12:06:30 -0700

    its a digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,17 +1,13 @@
 #include "main.h"
-
 /**
- * _isdigit - chrcks if c is digit
+ * _isdigit - checks if c is digit
  * @c: input parameter to check against
- * Return: always 0
+ * Return: 1 if its a digit and 0 if otherwise
  */
-
 int _isdigit(int c)
 {
-	c = 48;
-	if (c <= 57)
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
-	return (0);
 }
