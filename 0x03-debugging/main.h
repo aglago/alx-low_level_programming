c1d8b58b4059ac01436e88bd6c369946b8873874
amimanye <amimanyeaglago@gmail.com>
2023-06-22 18:30:35 -0700

    debugging

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+int convert_day(int day, int month);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+#endif
