ba925c197dfbe03cba7756db0a86a81fcafc695c
amimanye <amimanyeaglago@gmail.com>
2023-06-24 09:54:58 -0700

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,11 +6,10 @@
  */
 int print_last_digit(int n)
 {
-	int last_num;
+	int last_digit = n % 10;
 
-	if (n < 0)
-		n = -n;
-	last_num = n % 10;
-	_putchar(last_num + '0');
-	return (last_num);
+	if (last_digit < 0)
+		last_digit = -last_digit;
+	_putchar(last_digit + '0');
+	return (last_digit);
 }
