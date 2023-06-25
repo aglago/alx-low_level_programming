a55c039089e5a11e5610761da5997890ab1e1844
amimanye <amimanyeaglago@gmail.com>
2023-06-25 08:56:42 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * times_table - prints times table from 0 to 9
+ */
+void times_table(void)
+{
+	int num, t_num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		for (t_num = 0; t_num <= 9; t_num++)
+		{
+			int ans = num * t_num;
+
+			if (ans < 10)
+			{
+				_putchar(ans + '0');
+			}
+			else
+			{
+				_putchar(ans / 10 + '0');
+				_putchar(ans % 10 + '0');
+			}
+			if (t_num < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+		}
+		_putchar('\n');
+	}
+}
