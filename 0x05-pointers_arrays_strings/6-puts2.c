77f2fa6d8a1c6c86edfc530422fa20753d868f7f
amimanye <amimanyeaglago@gmail.com>
2023-06-26 22:11:05 -0700

    files

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,6 +8,7 @@
 void puts2(char *str)
 {
 	int len, i;
+
 	len = 0;
 	while (str[len] != '\0')
 	{
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,6 +9,7 @@
 void print_array(int *a, int n)
 {
 	int i;
+
 	for (i = 0; i < n; i++)
 	{
 		if (i == 0)
