9ae0d9fef8fae6f6cab616b5a93e51e6e90679ee
amimanye <amimanyeaglago@gmail.com>
2023-06-26 20:09:17 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - prints string to stdout
+ * @str: string
+ */
+void _puts(char *str)
+{
+	int index = 0;
+
+	while (str[index] != '\0')
+	{
+		_putchar(str[index]);
+		index++;
+	}
+}
