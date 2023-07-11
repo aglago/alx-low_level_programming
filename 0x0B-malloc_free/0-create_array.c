f2582ae9addb3aa0ace01f6723fa8d533023ed79
Ami Manye <amimanyeaglago@gmail.com>
2023-07-11 09:17:51 +0000

    return a locally created array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,13 +12,13 @@
 
 char *create_array(unsigned int size, char c)
 {
-	int i;
+	unsigned int i;
+	char *array;
 
 	if (size == 0)
 		return (NULL);
 
-	char *array = malloc(size * sizeof(char));
-
+	array = malloc(size * sizeof(char));
 	if (array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
