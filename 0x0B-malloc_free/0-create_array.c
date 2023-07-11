a9c8f2e4e112e7c62fb46435daffed7242ee8e35
Ami Manye <amimanyeaglago@gmail.com>
2023-07-11 08:49:21 +0000

    return a locally created array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * create_array - creates a local array
+ *
+ * desc: using malloc, I am able to return a pointer to a locally created array
+ * Return: returns the locally created array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+		return (NULL);
+	else
+	{
+		char *array = malloc(size * sizeof(char));
+		if (array == NULL)
+			return (NULL);
+		else
+		{
+			*array = c;
+			return (array);
+		}
+	}
+}
