c326e202a2d838e869b70efb619acdb7f60ce811
amimanye <amimanyeaglago@gmail.com>
2023-07-18 03:14:37 -0700

    program that prints name of file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * desc: pribts program name fron
+ * which the executable was compiled
+ * from
+ *
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
