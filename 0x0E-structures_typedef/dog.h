7a9faf82e6774a28c7c6306e49afd0b1c33512de
Ami Manye <amimanyeaglago@gmail.com>
2023-07-18 20:39:57 +0000

    added prototype to header file && defined new name for struct using typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,11 +8,14 @@
  * @age: age of dog
  * @owner: owner of dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
