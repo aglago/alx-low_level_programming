4ce4ba610d80c1aff1fdf0d6497c6986c94ef7fe
Ami Manye <amimanyeaglago@gmail.com>
2023-07-19 19:23:48 +0000

    print name and header file added

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ *
+ * @name: the name to be printed
+ * @f: function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,4 @@
+#ifndef FPTR_H
+#define FPTR_H
+void print_name(char *name, void (*f)(char *));
+#endif
