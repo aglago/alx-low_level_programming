3a14a58ff90942d9c79ca4c2fd8073d7244ccf61
Ami Manye <amimanyeaglago@gmail.com>
2023-07-19 20:21:00 +0000

    execute function to array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates and executes 
+ * function pointer
+ *
+ * @array: array
+ * @size: size of array
+ * @action: functon pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
