c305a677cbf7c837bded67f19079e3b2b3550524
Ami Manye <amimanyeaglago@gmail.com>
2023-07-19 20:44:04 +0000

    set size_t

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,6 @@
 #ifndef FPTR_H
 #define FPTR_H
+typedef unsigned long int size_t;
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
