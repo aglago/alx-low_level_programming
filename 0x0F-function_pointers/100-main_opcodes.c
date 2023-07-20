c50256dde0e4bc1d161874f508eeb195437f2944
Ami Manye <amimanyeaglago@gmail.com>
2023-07-20 01:31:02 +0000

    prints opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,39 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_opcodes(int num_bytes) {
-	asm volatile(
-			"mov %0, %%rax\n"
-			"mov $0x4005f6, %%rdi\n"
-			"loop_start:\n"
-			"cmp $0, %%rax\n"
-			"je loop_end\n"
-			"dec %%rax\n"
-			"movzb (%%rdi), %%eax\n"
-			"printf \"%.2x \", %%eax\n"
-			"inc %%rdi\n"
-			"jmp loop_start\n"
-			"loop_end:\n"
-			:
-			: "r" (num_bytes)
-			: "%rax", "%rdi", "%eax", "memory"
-		    );
-}
+/**
+ * main - Entry point of the program
+ * @argc: The number of arguments (including the program name)
+ * @argv: Array of pointers to strings containing the arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, num_bytes;
 
-int main(int argc, char *argv[]) {
-	if (argc != 2) {
+	if (argc != 2)
+	{
 		printf("Error\n");
 		return 1;
 	}
 
-	int num_bytes = atoi(argv[1]);
+	num_bytes = atoi(argv[1]);
 
-	if (num_bytes < 0) {
+	if (num_bytes < 0)
+	{
 		printf("Error\n");
 		return 2;
 	}
 
-	print_opcodes(num_bytes);
+	/* Print the opcodes */
+	unsigned char *ptr = (unsigned char *)main;
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02x ", *ptr);
+		ptr++;
+	}
 	printf("\n");
 
 	return 0;
