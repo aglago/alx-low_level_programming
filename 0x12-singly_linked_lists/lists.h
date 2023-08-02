be8bf807108ece02f7e96b3ce5f8e6537e81ee1c
Ami Manye <amimanyeaglago@gmail.com>
2023-08-02 18:58:38 +0000

    betty for header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -9,14 +9,16 @@
  *
  * Description: singly linked list node structure
  */
+
+#include <stddef.h>
+#include <stdio.h>
+
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
-
-
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
