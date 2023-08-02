abbe25b08930cefdc413ee62792d6b88c916d784
Ami Manye <amimanyeaglago@gmail.com>
2023-08-02 23:50:07 +0000

    betty fixed

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,29 +2,29 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-    char *new_str = strdup(str);
-    list_t *new_node ;
+	char *new_str = strdup(str);
+	list_t *new_node ;
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL) /* if memory allocation fails, return NULL */
-        return (NULL);
-
-
-    new_node -> str = new_str;
-    if (new_node -> str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
-
-    new_node -> len = strlen(str);
-    new_node -> next = *head;
-    *head = new_node;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL) /* if memory allocation fails, return NULL */
+		return (NULL);
 
 
-    return (new_node);
+	new_node -> str = new_str;
+	if (new_node -> str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    /** if malloc fails, we have to free something
-     * if strdup fails, we have to free something.
-     */
+	new_node -> len = strlen(str);
+	new_node -> next = *head;
+	*head = new_node;
+
+
+	return (new_node);
+
+	/** if malloc fails, we have to free something
+	 * if strdup fails, we have to free something.
+	 */
 }
