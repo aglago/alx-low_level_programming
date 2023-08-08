8a8f9a8105ef11941403e1f106f52a0a684d74b2
Ami Manye <amimanyeaglago@gmail.com>
2023-08-08 11:17:25 +0000

    function to print binary of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - prints a number in binary
+ * @n: unsigned long integer to print it's binary
+ * Return: nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i, j, arr[98], remain, count = 0;
+
+	i = 0;
+	do {
+		remain = n % 2;
+		arr[i] = remain;
+		n = n / 2;
+		i++;
+		count++;
+	} while (n);
+
+	while (count)
+	{
+		printf("%d", arr[count - 1]);
+		count--;
+	}
+}
