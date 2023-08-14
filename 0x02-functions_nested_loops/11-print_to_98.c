1f97eec60e275dd3043485120b60dd403559f616
Ami Manye <amimanyeaglago@gmail.com>
2023-08-14 11:27:06 +0000

    print numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,89 +1,35 @@
+#include <stdio.h>
 #include "main.h"
 
-/**
- * print_to_98 - prints natural numbers
- * from n to 98
- *
- * @n: starting point
- */
 
+/**
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by a new line
+ * @n: print from this number
+ */
 void print_to_98(int n)
 {
-	/* declaring variables */
-	int i, j, neg;
+	int i, j;
 
-	/* if n is greater than 98 */
-	if (n > 98)
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	}
+	else if (n >= 98)
 	{
 		for (j = n; j >= 98; j--)
 		{
-			if (j < 100)
-			{
-				_putchar('0' + j / 10);
-				_putchar('0' + j % 10);
-			} else
-				if (j < 1000)
-				{
-					_putchar ('0' + j / 100);
-					_putchar('0' + (j % 100) / 10);
-					_putchar('0' + (j % 100) % 10);
-				}
-
-			if(j > 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
 		}
 	}
-	else
-	{
-		/* if n is less than 98 */
-		for (i = n; i < 99; i++)
-		{
-			/* if number is negative */
-			if (i < 0)
-			{
-				/* make number positive */
-				neg = -i;
-
-				/* prints a - */
-				_putchar('-');
-
-				/**
-				 * if number is greater than 9
-				 * print the first number
-				 * else 0 will be printed if 
-				 * number is less than 9
-				 * example 01, 02, 03
-				 */
-				if (neg > 99)
-				{
-					_putchar('0' + neg / 100);
-					_putchar('0' + (neg % 100) / 10);
-					_putchar('0' + (neg % 100) % 10);
-				}
-				else
-				{
-					if (neg > 9)
-						_putchar('0' + neg / 10);
-					_putchar('0' + neg % 10);
-				}
-			}
-			else if (i < 10 && i >= 0)
-				_putchar('0' + i);
-			else
-			{
-				_putchar('0' + i / 10);
-				_putchar('0' + i % 10);
-			}
-
-			if (i < 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-	}
-	_putchar('\n');
 }
