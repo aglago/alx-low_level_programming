6313f85249b6757f9cd0bb24dfc46ed563097ce2
Ami Manye <amimanyeaglago@gmail.com>
2023-11-04 00:45:47 +0000

    added prototypes of task 3

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -2,6 +2,7 @@
 #define HASHTABLES_H
 
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -37,5 +38,7 @@ hash_table_t *hash_table_create(int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+hash_node_t *create_item(const char *key, const char *value);
+void handle_collision(hash_table_t *table, hash_node_t *item, int index);
 
 #endif
