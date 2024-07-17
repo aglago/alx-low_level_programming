b007e84069abdff8bb6ee8fe3d558c573decc919
Samuella M. Aglago <samuellamanyeaglago@gmail.com>
2024-07-17 17:22:55 +0000

    🎨 Edit: add parenthesis around return value

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -26,7 +26,7 @@ int binary_search(int *array, size_t size, int value)
 		mid = low + (high - low) / 2;
 
 		if (array[mid] == value)
-			return mid;
+			return (mid);
 		if (array[mid] < value)
 			low = mid + 1;
 		else
